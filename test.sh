a2911dcf6ed7ee2cbc9c3598fb2336981f1ad06a
Maddy Alj <maddy.alj@gmail.com>
2016-10-18 23:12:50 +0100

    Changed test script to pass -e flag if test filename contained *.e_ExpectedValue.*

--- test.sh
+++ test.sh
@@ -1,10 +1,25 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo ""
+printf "\n"
 for file in tests/*
 do
     printf "${bold}## Testing $(basename $file)${normal}\n"
-    ./tester.native $file
-    echo ""
+    if [[ $file == *.e_*.* ]]
+    then
+        expected=${file#*.e_}
+        expected=${expected%.*}
+        printf "Expected value = %d\n" $expected
+        output=$(./tester.native $file -e)
+        printf "Actual value   = $output\n"
+        if [[ $output == $expected ]]
+        then
+            printf "√ Correct\n"
+        else
+            printf "× Wrong\n"
+        fi
+    else
+        ./tester.native $file
+    fi
+    printf "\n"
 done
