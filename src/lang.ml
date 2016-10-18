e616a1f7bd048138e6e59d9180f44a334f65ad17
Maddy Alj <maddy.alj@gmail.com>
2016-10-18 21:53:54 +0100

    Added evaluation functions to Lang

--- src/lang.ml
+++ src/lang.ml
@@ -44,7 +44,7 @@ let op_str = function
     | Or     -> "Or"
     | Not    -> "Not"
 
-open Core.Std
+open Core.Std.Printf
 let rec exp_str = function
     | If (e1, e2, e3)   -> sprintf "If (%s, %s, %s)" (exp_str e1) (exp_str e2) (exp_str e3)
     | While (e1, e2)    -> sprintf "While (%s, %s)" (exp_str e1) (exp_str e2)
@@ -53,7 +53,7 @@ let rec exp_str = function
     | Asg (e1, e2)      -> sprintf "Asg (%s, %s)" (exp_str e1) (exp_str e2)
     | Op (op, e1, e2)   -> sprintf "Op (%s, %s, %s)" (op_str op) (exp_str e1) (exp_str e2)
     | Print e           -> sprintf "Print (%s)" (exp_str e)
-    | Appl (s, es)      -> sprintf "Appl (\"%s\", %s)" s ("[" ^ (String.concat ~sep:", " (List.map ~f:exp_str es)) ^ "]")
+    | Appl (s, es)      -> sprintf "Appl (\"%s\", %s)" s ("[" ^ (String.concat ", " (List.map exp_str es)) ^ "]")
     | Deref e           -> sprintf "Deref (%s)" (exp_str e)
     | Val n             -> sprintf "Val %d" n
     | Id x              -> sprintf "Id \"%s\"" x
@@ -61,5 +61,36 @@ let rec exp_str = function
     | Empty             -> sprintf "Empty"
 
 let prog_str p =
-    let func_str (f, ps, e) = f ^ "(" ^ (String.concat ~sep:", " ps) ^ "):\n    " ^ (exp_str e) ^ "\n" in
-        String.concat (List.map ~f:func_str p)
+    let func_str (f, ps, e) = f ^ "(" ^ (String.concat ", " ps) ^ "):\n    " ^ (exp_str e) ^ "\n" in
+        String.concat "" (List.map func_str p)
+
+let store = Hashtbl.create 100
+let rec eval_exp =
+    let bool_int b = if b then 1 else 0
+    and exp_bool e = if (eval_exp e) > 0 then true else false in function
+        | If (e1, e2, e3)     -> if exp_bool e1 then eval_exp e2 else eval_exp e3
+        | While (e1, e2)      -> while exp_bool e1 do (let _ = eval_exp e2 in ()) done; 0
+        | Const (s, e1, e2)   -> let v2 = eval_exp e1 in Hashtbl.replace store s v2; eval_exp e2
+        | Let (s, e1, e2)     -> let v2 = eval_exp e1 in Hashtbl.replace store s v2; eval_exp e2
+        | Asg (Id x, e2)      -> let v2 = eval_exp e2 in Hashtbl.replace store x v2; 0
+        | Op (Plus,   e1, e2) -> eval_exp e1 + eval_exp e2
+        | Op (Minus,  e1, e2) -> eval_exp e1 - eval_exp e2
+        | Op (Times,  e1, e2) -> eval_exp e1 * eval_exp e2
+        | Op (Divide, e1, e2) -> eval_exp e1 / eval_exp e2
+        | Op (Lteq,   e1, e2) -> (eval_exp e1 <= eval_exp e2) |> bool_int
+        | Op (Lt,     e1, e2) -> (eval_exp e1 <  eval_exp e2) |> bool_int
+        | Op (Gteq,   e1, e2) -> (eval_exp e1 >= eval_exp e2) |> bool_int
+        | Op (Gt,     e1, e2) -> (eval_exp e1 >  eval_exp e2) |> bool_int
+        | Op (Eq,     e1, e2) -> (eval_exp e1 == eval_exp e2) |> bool_int
+        | Op (Noteq,  e1, e2) -> (eval_exp e1 != eval_exp e2) |> bool_int
+        | Op (And,    e1, e2) -> (exp_bool e1 && exp_bool e2) |> bool_int
+        | Op (Or,     e1, e2) -> (exp_bool e1 || exp_bool e2) |> bool_int
+        | Op (Not,    e,  _ ) -> exp_bool e |> not            |> bool_int
+        | Deref (Id x)        -> Hashtbl.find store x
+        | Val n               -> n
+        | Seq (e1, e2)        -> let _ = eval_exp e1 in let v2 = eval_exp e2 in v2
+        | _                   -> 0
+
+let rec eval_prog = function
+    | [] -> 0
+    | (f, _, e) :: fs -> if (String.compare f "main") == 0 then eval_exp e else eval_prog fs
